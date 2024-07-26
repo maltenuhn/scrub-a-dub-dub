3c88b981e6a79f34b831140b5b0bde7f51e962b3
Malte <malte.nuhn@gmail.com>
2024-07-26 16:55:28 +0200

    fourth blood

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/scrub-a-dub-dub/", // Replace <REPO> with your repository name
+});
